333566db2170160c94d5ce8fa3f8978215eb45df
afreij <atfreij@ncsu.edu>
2018-07-26 15:43:16 -0400

    Corrected behavior where 'a' node was being added twice, corrected traversal check from current->next != NULL to current != NULL, completed doubly linked list traversal and letter lookup

--- src/main.cpp
+++ src/main.cpp
@@ -32,28 +32,33 @@ int main() {
   node *head;
   node *current;
   node *previous;
+  node *tmp; 
 
-  struct node *list = new node;
-  list->letter = letter;
-  list->next = NULL;
-  list->prev = NULL;
-  head = list;
+  struct node *list;
+  //list->letter = letter;
+  //list->next = NULL;
+  //list->prev = NULL;
+  //head = list;
 
-  current = list;
-  previous = list;
+  //current = list;
+  //previous = list;
 
   //char conversion and increment
   int int_data = letter;
   cout << "Letter to be searched for: ";
   cin >> search;
 
-  for(i = 0; i < len; i++) {
+  for(i = 1; i <= len; i++) {
     // set previous to point to current node for it to become the previous node
     struct node *new_node = new node;
+    if (i == 1) { 
+      list = new_node;
+      head = list;
+    }
     new_node->next = NULL;
     new_node->prev = NULL;
-    current = new_node;
-    if (i != 0) {
+    current = list;
+    if (i != 1) {
       // list is the first node of the list, no need to have two "first" nodes
       int_data += 1;
     } //end if
@@ -63,16 +68,15 @@ int main() {
     new_node->prev = list;
     list = list->next;
     current = list;
-    cout << "Value of node #" << i << ": " << list->letter << endl; 
-    //}
-    //int_a += 1;
-    //new_node->letter = (char) int_a;
-    //new_node->next = NULL;
-    //new_node->prev = NULL;
-    //current = new_node;
-    //current->prev 
+    cout << "Value of node #" << i << ": " << list->letter << endl;  
   }
- // cout << "Hello world!\n";
+
+  // debugging traversal
+//  tmp = head;  
+//  while (tmp != NULL) {
+//    cout << tmp->letter << endl;
+//    tmp = tmp->next;
+//  }
 
   traversal(head, list, search);
   cout << "Size of malloc_alphabet: " << sizeof(malloc_alphabet) << endl;
@@ -85,18 +89,29 @@ int main() {
 void traversal(node* head, node* tail, char search) {
   struct node* current = head;
   struct node* end = tail;
+  int forward = 1;
+  int reverse = 1;
   
   //testing out standard traversal, head->tail
-  while (current->next != NULL) {
-    cout << "I found " << current->letter << " here!" << endl;
+  while (current != NULL) {
+    //cout << "I found " << current->letter << " here!" << endl;
     if (current->letter == search){
-      cout << search << " found at memory location " << &(current->letter) << endl;
+      cout << "Forward search: " << search << " found at memory location " << &current 
+        << " after " << forward << " jumps!" << endl;
       break;
     } else {
+      forward += 1;
       current = current->next;
     }
   }
 
-
-  // test reverse traversal, tail->head, to see if prev fields have been filled in correctly
+  while(end != NULL) {
+    if (end->letter == search) {
+      cout << "Reverse search: " << search << "found at " << &current << " after " << reverse << " jumps!" <<endl;
+      break;
+    } else {
+      reverse += 1;
+      end = end->prev;
+    }
+  }
 }
